de8aaea01c913e8dbecd67f4d8c909061d7353b9
Rodrigo Banegas <banegasrodrigo89@gmail.com>
2026-09-17 16:04:17 -0300

    Bump version to 1.0.2 (code 3)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -66,8 +66,8 @@ android {
         applicationId = "ar.com.longcasting"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
     packaging {
         resources {
