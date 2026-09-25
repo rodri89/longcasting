aaa731dd78aa3fd211b2ef8ca9acbbd0dd6eeed0
Rodrigo Banegas <banegasrodrigo89@gmail.com>
2026-09-25 13:44:24 -0300

    Fix: Disable WorkManager initialization to prevent crash (v1.0.6)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -66,8 +66,8 @@ android {
         applicationId = "ar.com.longcasting"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
     }
     packaging {
         resources {
